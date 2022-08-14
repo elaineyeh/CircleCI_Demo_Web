e45334579d4e5f6cee52a03b01f306e880e0f44b
elaineyeh <elaine81206@gmail.com>
2022-08-14 09:36:36 +0800

    fix: project name in config.yml

--- CircleCI_Demo_Web/quickstart/tests.py
+++
@@ -1,9 +0,0 @@
-from urllib import response
-from django.test import TestCase
-
-
-class TestHomePageCase(TestCase):
-    def test_home_page(self):
-        response = self.client.get('/home/')
-
-        self.assertEqual(response.status_code, 204)
--- CircleCI_Demo_Web/quickstart/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def home_view(request, **kwargs):
-    return HttpResponse("CICD Demo", status=200)
